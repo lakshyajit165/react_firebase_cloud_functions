e1b9a1b787b62540ca6d7c1adf3aaac8f56fc941
LAKSHYAJIT LAXMIKANT <llaxmik1@in.ibm.com>
2019-07-10 12:22:42 +0530

    Updates

---
+++ precache-manifest.8e5598bf3947e4ac30aa829456885ef8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe02a5d9b1acc0e51eb688f5c66cccd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ca531c78271e2398723",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "283f1bc7de4307228c53",
+    "url": "/static/js/2.8374892b.chunk.js"
+  },
+  {
+    "revision": "1ca531c78271e2398723",
+    "url": "/static/js/main.21d11b58.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
